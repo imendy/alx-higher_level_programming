edbd7002cd272361ce5e5026fb5335bf96d1c82b
EMMANUEL NDUKWE NMAJU <enmaju37@gmail.com>
2023-06-08 04:27:29 -0700

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import addition, subtract, multiply, divide
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, addition(a, b)))
+    print("{} - {} = {}".format(a, b, subtract(a, b)))
+    print("{} * {} = {}".format(a, b, multiply(a, b)))
+    print("{} / {} = {}".format(a, b, divide(a, b)))
