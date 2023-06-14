9c4d789e26c17228e9444df0571f5b4d5e3f6de5
EMMANUEL NDUKWE NMAJU <enmaju37@gmail.com>
2023-06-14 05:36:27 -0700

    updated

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,4 @@
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for item in my_list:
-        if item == search:
-            new_list.append(replace)
-        else:
-            new_list.append(item)
-    return new_list
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
