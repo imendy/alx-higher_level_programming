9e55d943b7ae86bff2d6b76db2a08fddefe8b862
EMMANUEL NDUKWE NMAJU <enmaju37@gmail.com>
2023-06-14 06:14:55 -0700

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary.get(key) is not None:
+    if key in a_dictionary:
         del a_dictionary[key]
-    return (a_dictionary)
+    return a_dictionary
 
