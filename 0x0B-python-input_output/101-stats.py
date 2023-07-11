85277b6590ea7fbe6d1b3e83a3ce5301bd8f528e
EMMANUEL NDUKWE NMAJU <enmaju37@gmail.com>
2023-07-11 09:36:00 -0700

    updated

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,55 +3,55 @@
 import sys
 
 
-class StatusCodeAnalyzer:
-    """ Class to generate instances with dictionary and size"""
+class Magic:
+    """ Class to generates instances with dict and size"""
     def __init__(self):
         """ Init method """
-        self.status_dict = {}
+        self.dic = {}
         self.size = 0
 
-    def init_status_dict(self):
-        """ Initialize dictionary """
-        self.status_dict['200'] = 0
-        self.status_dict['301'] = 0
-        self.status_dict['400'] = 0
-        self.status_dict['401'] = 0
-        self.status_dict['403'] = 0
-        self.status_dict['404'] = 0
-        self.status_dict['405'] = 0
-        self.status_dict['500'] = 0
+    def init_dic(self):
+        """ Initialize dict """
+        self.dic['200'] = 0
+        self.dic['301'] = 0
+        self.dic['400'] = 0
+        self.dic['401'] = 0
+        self.dic['403'] = 0
+        self.dic['404'] = 0
+        self.dic['405'] = 0
+        self.dic['500'] = 0
 
     def add_status_code(self, status):
-        """ Add repeated number to the status code """
-        if status in self.status_dict:
-            self.status_dict[status] += 1
+        """ add repeated number to the status code """
+        if status in self.dic:
+            self.dic[status] += 1
 
     def print_info(self, sig=0, frame=0):
-        """ Print status code """
+        """ print status code """
         print("File size: {:d}".format(self.size))
-        for key in sorted(self.status_dict.keys()):
-            if self.status_dict[key] != 0:
-                print("{}: {:d}".format(key, self.status_dict[key]))
+        for key in sorted(self.dic.keys()):
+            if self.dic[key] is not 0:
+                print("{}: {:d}".format(key, self.dic[key]))
 
 
 if __name__ == "__main__":
-    analyzer = StatusCodeAnalyzer()
-    analyzer.init_status_dict()
+    magic = Magic()
+    magic.init_dic()
     nlines = 0
 
     try:
         for line in sys.stdin:
-            if nlines % 10 == 0 and nlines != 0:
-                analyzer.print_info()
+            if nlines % 10 == 0 and nlines is not 0:
+                magic.print_info()
 
             try:
                 list_line = [x for x in line.split(" ") if x.strip()]
-                analyzer.add_status_code(list_line[-2])
-                analyzer.size += int(list_line[-1].strip("\n"))
+                magic.add_status_code(list_line[-2])
+                magic.size += int(list_line[-1].strip("\n"))
             except:
                 pass
             nlines += 1
     except KeyboardInterrupt:
-        analyzer.print_info()
+        magic.print_info()
         raise
-    analyzer.print_info()
+    magic.print_info()
