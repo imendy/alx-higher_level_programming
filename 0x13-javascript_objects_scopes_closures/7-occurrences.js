1d3346585a00d5e1498b7f86096d3551ef279ba7
Emmanuel Ndukwe Nmaju <enmaju37@gmail.com>
2023-09-12 15:40:42 -0700

    updated

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  let count = 0;
-  for (let i in list) {
-    if (list[i] === searchElement) {
-      count++;
+  let nOccurrences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      nOccurrences++;
     }
   }
-  return count;
+  return nOccurrences;
 };
