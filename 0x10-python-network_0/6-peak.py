830be2b8f7f2f819fa8955105767274f7ec64872
Emmanuel Ndukwe Nmaju <enmaju37@gmail.com>
2023-09-28 13:25:17 -0700

    updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""
-Find Peak task 6
-"""
+"""Defines a peak-finding algorithm."""
+
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
+    """ Finds the peak in a list of integers """
     if list_of_integers == []:
         return None
 
-    list_length = len(list_of_integers)
-    if list_length == 1:
-        return list_of_integers[0]
-    elif list_length == 2:
-        return max(list_of_integers)
+    length = len(list_of_integers)
+    m = int(length / 2)
+    li = list_of_integers
 
-    mid = int(list_length / 2)
-    current_element = list_of_integers[mid]
-    if current_element > list_of_integers[mid - 1] and current_element > list_of_integers[mid + 1]:
-        return current_element
-    elif current_element < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    if m - 1 < 0 and m + 1 >= length:
+        return li[m]
+    elif m - 1 < 0:
+        return li[m] if li[m] > li[m + 1] else li[m + 1]
+    elif m + 1 >= length:
+        return li[m] if li[m] > li[m - 1] else li[m - 1]
+
+    if li[m - 1] < li[m] > li[m + 1]:
+        return li[m]
+
+    if li[m + 1] > li[m - 1]:
+        return find_peak(li[m:])
+    return find_peak(li[:m])
