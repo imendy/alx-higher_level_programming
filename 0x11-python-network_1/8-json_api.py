c62485b15d9b5532552adeeedd1412bc5c667e8a
Emmanuel Ndukwe Nmaju <enmaju37@gmail.com>
2023-09-29 14:36:51 -0700

    more update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
 """
-Module  sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter.
+Python script that takes in a letter
+and sends a POST request
 """
-import requests
 from sys import argv
+import requests
 
-
-def main(argv):
-    """
-    Script that takes in a letter and sends a POST request to
-    http://0.0.0.0:5000/search_user with the letter as a parameter.
-    """
-    if len(argv) < 2:
-        q = ""
-    else:
-        q = argv[1]
-    payload = {'q': q}
-    url = "http://0.0.0.0:5000/search_user"
-    r = requests.post(url, data=payload)
-    try:
-        result = r.json()
-        if bool(result) is False:
-            print("No result")
-        else:
-            print("[{}] {}".format(result['id'], result['name']))
-    except:
-        print("Not a valid JSON")
 
 if __name__ == "__main__":
-    main(argv)
+    arg = ""
+    if len(argv) == 2:
+        arg = argv[1]
+    try:
+        r = requests.post('http://0.0.0.0:5000/search_user', data={'q': arg})
+        j = r.json()
+        if j:
+            print('[{}] {}'.format(j.get('id'), j.get('name')))
+        else:
+            print('No result')
+    except ValueError:
+        print('Not a valid JSON')
